60505377d157b0f23903592c0da4e67a21ed0f50
Aaron Bjork <aaronbjork@hotmail.com>
2018-10-22 15:11:02 -0700

    Addding the data call

---
+++ src/Scorecard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Scorecard.css';
+
+class Scorecard extends Component {
+  render() {
+    return (
+      <div className="Scorecard">
+        {this.state.greeting} {this.props.name}!
+      </div>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { greeting: 'Hello' };
+
+  }
+
+  async componentDidMount() {
+    const url = "https://tgsc-api.azurewebsites.net/api/values";  // https://topgunapi.azurewebsites.net/api/course/6, https://randomuser.me/api/?results=2
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+
+  }
+}
+
+
+
+export default Scorecard;
+
